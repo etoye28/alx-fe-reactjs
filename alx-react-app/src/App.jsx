d9942d2649b631fb4ab49470282da7d2242b6305
Etsub Berihun <etsubberihun09@gmail.com>
2025-08-28 07:31:16 -0700

    ITTW

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,24 +1,25 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
+    <WelcomeMessage />
     <div>
-    <Header />
+      <Header />
       <MainContent />
       <Footer />
-      </div>
-    <WelcomeMessage />
-    
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
