998018f3a15b93972f14c814e6ec5e9a9a9004e4
etoye <etsubberihun09@gmail.com>
2025-10-10 19:33:37 +0100

    wthit

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App p-6">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
