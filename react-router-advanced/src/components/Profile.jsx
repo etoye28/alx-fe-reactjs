1cf5553eb29be58c3124888482610c6154fc1d26
etoye <etsubberihun09@gmail.com>
2025-10-10 18:29:49 +0100

    tysmG

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile p-6 text-center">
+      <h1 className="text-2xl font-bold">Profile</h1>
+      <p>This is the Profile component.</p>
+    </div>
+  );
+};
+
+export default Profile;
