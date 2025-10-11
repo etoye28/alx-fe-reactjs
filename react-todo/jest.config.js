aabe5cf422fde83757b3c640cb322a2826494b63
etoye <etsubberihun09@gmail.com>
2025-10-11 19:26:07 +0100

    wwwu

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,4 +1,8 @@
 export default {
   testEnvironment: "jsdom",
-  transform: {},
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  moduleFileExtensions: ["js", "jsx"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
